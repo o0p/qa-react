2a590a41b62eb02c0796cd4c96b1f28f27ae0a5c
Ruslan <rus@gmx.com>
2021-05-06 16:20:26 +0100

    Add info state

--- src/Question.js
+++ src/Question.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ title, info }) => {
+  const [info, setInfo] = useState(true);
   return (
     <>
       <div className="question">
         <header>
           <h4>{title}</h4>
+          <button className="btn">btn</button>
         </header>
       </div>
     </>
