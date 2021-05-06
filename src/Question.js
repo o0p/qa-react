06e16d2bd26c87c5a5967e7a411644821513286b
Ruslan <rus@gmx.com>
2021-05-06 16:39:55 +0100

    Remove empty markers

--- src/Question.js
+++ src/Question.js
@@ -3,21 +3,19 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ title, info }) => {
   const [displayInfo, setDisplayInfo] = useState(false);
   return (
-    <>
-      <div className="question">
-        <header>
-          <h4>{title}</h4>
-          <button
-            className="btn"
-            onClick={() => {
-              setDisplayInfo(!displayInfo);
-            }}>
-            {displayInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-          </button>
-        </header>
-        {displayInfo && <p>{info}</p>}
-      </div>
-    </>
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setDisplayInfo(!displayInfo);
+          }}>
+          {displayInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {displayInfo && <p>{info}</p>}
+    </div>
   );
 };
 export default Question;
