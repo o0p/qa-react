37a37f91b3794184ecc81e952ba9df47ffa7c833
Ruslan <rus@gmx.com>
2021-05-06 16:41:41 +0100

    Remove unnecessary tags

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ function App() {
   const [questions, setQuestions] = useState(data);
   return (
     <main className="container">
-      <div className="container">
-        <h3>Questions and Answers about Login</h3>
-        <section className="info">
-          {questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question} />;
-          })}
-        </section>
-      </div>
+      <h3>Questions and Answers about Login</h3>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
     </main>
   );
 }
